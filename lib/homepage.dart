4e7c920aad22a51851586c7f7ca652b4c2351353
unknown <azazahmadkhan@gmail.com>
2025-02-04 23:32:27 +0500

    slider widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:towards_excellence/posts/post1.dart';
-import 'package:towards_excellence/posts/post2.dart';
 
-class Homepage extends StatelessWidget {
-  Homepage({super.key});
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
 
-  final _controller = PageController();
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
 
+class _HomepageState extends State<Homepage> {
+  String post = '';
+  final textcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('STILL LEARNING'),
-          backgroundColor: Colors.cyan,
-          centerTitle: true,
-        ),
-        body: PageView(
-          // scrollDirection: Axis.vertical,
-          controller: _controller,
-          children: [Mypost1(), Mypost2()],
-        ));
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 300,
+            width: 200,
+            child: Text(
+              post,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: textcontroller,
+                decoration: InputDecoration(
+                    hintText: "What's on your mind",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    suffix: IconButton(
+                        onPressed: () {
+                          textcontroller.clear();
+                        },
+                        icon: Icon(Icons.clear))),
+              )),
+          MaterialButton(
+            onPressed: () {
+              setState(() {
+                post = textcontroller.text;
+              });
+            },
+            child: Text('Tweet'),
+            color: Colors.amber,
+          )
+        ],
+      ),
+    );
   }
 }

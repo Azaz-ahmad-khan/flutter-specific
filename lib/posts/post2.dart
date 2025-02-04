48934574afa457d452714bd5b6d73b08ebca6f9e
unknown <azazahmadkhan@gmail.com>
2025-02-04 20:00:35 +0500

    this is the small usage of textfield

---
+++ lib/posts/post2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Mypost2 extends StatelessWidget {
+  const Mypost2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
